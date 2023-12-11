ca2a96fdda12a8bd976fbcb0334b32e204a3f8cb
wdShopnil <shopnilabdullah2001@gmail.com>
2023-12-12 04:04:34 +0600

    particle Added

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Outlet } from 'react-router-dom'
-import Navbar from './Components/Navbar'
+
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoCloseSharp } from "react-icons/io5";
+import Home from './Components/Home'
 function App() {
   const [isOpen, setIsOpen] = useState(false)
 
@@ -15,17 +16,24 @@ function App() {
     <div className='text-xl rounded-xl hover:border-orange-300 lg:hidden hover:border-2 w-9 h-9 absolute left-20'>
     <button onClick={()=>setIsOpen(true)}><GiHamburgerMenu className='w-8 h-8 mx-auto' /></button>
     </div>
-   <div  className={`min-h-screen absolute top-0 lg:w-96 bg-slate-800 z-10 border-2 duration-1000   border-stone-700 w-60 ${isOpen? 'left-0':'-left-96' }`}>
-    <div className=' relative lg:hidden'>
+   <div  className={`min-h-screen absolute top-0 lg:w-96 bg-slate-800 z-10 border-2 duration-1000 md:left-0 lg:left-0  border-stone-700 w-60 ${isOpen? 'left-0':'-left-96' }`}>
+    <div className=' relative md:hidden lg:hidden'>
     <button onClick={()=>setIsOpen(false)} className='w-8 h-8 mr-0 absolute right-1 '><IoCloseSharp className='h-full w-full hover:w-7 text-slate-100 '></IoCloseSharp></button>
     </div>
-            
+            <div>
+              <img className='rounded-full w-4/5 mx-auto mt-5' src="../src/assets/profile.jpg" alt="" />
+              <h2 className='text-3xl font-bold '>Abdullah Aiyash Shopnil</h2>
+              <ul>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+              </ul>
+              </div>
      </div>
    </div>
       <Outlet></Outlet>
-      <div className={`${isOpen? 'opacity-60':''}`}>
-
-      </div>
+      <Home></Home>
+      
     </>
   )
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,123 @@
 import React from 'react';
 
+import { useCallback, useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import reactimage from '../assets/react.svg';
+
+//   import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
+ import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
+
+
 const Home = () => {
-    return (
-        <div>
+    const [ init, setInit ] = useState(false);
+
+    // this should be run only once per application lifetime
+    useEffect(() => {
+        initParticlesEngine(async (engine) => {
+            // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+            // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+            // starting from v2 you can add only the features you need reducing the bundle size
+            // await loadAll(engine);
+            await loadFull(engine);
+            await loadSlim(engine);
             
-        </div>
+            
+            //await loadBasic(engine);
+        }).then(() => {
+            setInit(true);
+        });
+    }, []);
+
+    const particlesLoaded = (container) => {
+        console.log(container);
+    };
+
+    return (
+        <div className='w-full'>
+           { init && <Particles
+            id="tsparticles"
+            particlesLoaded={particlesLoaded}
+            options={{
+                background: {
+                    color: {
+                        value: "",
+                    },
+                },
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: true,
+                            mode: "push",
+                        },
+                        onHover: {
+                            enable: true,
+                            mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 5,
+                        },
+                        repulse: {
+                            distance: 100,
+                            duration: 0.8,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#baeeed",
+                    },
+                    
+                    links: {
+                        color: "#39db94",
+                        distance: 100,
+                        enable: true,
+                        opacity: .7,
+                        width: 1,
+                    },
+                    
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 4,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 500,
+                        },
+                        value: 500,
+                    },
+                    opacity: {
+                        value: 0.8,
+                    },
+                    shape: {
+                        type: ["image","circle"],
+                        image: {
+                          src:reactimage
+                            
+                        }
+                      },
+                    size: {
+                        value: { min: 5, max: 10 }
+                    },
+                },
+                detectRetina: true,
+            }}
+        />
+}
+        
+        </div> 
     );
 };
 
